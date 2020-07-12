f7437a680878859ed3650c77ed318a1a1e64b37c
tta2yta@gmail.com <tta2yta@gmail.com>
2020-07-12 14:38:13 +0300

    deploy to azure 7/12/2020

--- Startup.cs
+++ Startup.cs
@@ -27,15 +27,15 @@ namespace OnlineCakeShop
         }
         public void ConfigureServices(IServiceCollection services)
         {
-            /*services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));*/
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
 
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+         /*   if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
             {
                 services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
-            }
+            }*/
 
 
             services.AddIdentity<IdentityUser, IdentityRole>(options =>
@@ -45,7 +45,8 @@ namespace OnlineCakeShop
                 options.Password.RequireUppercase = true;
                 options.User.RequireUniqueEmail = true;
 
-            })
+            }).AddDefaultUI()
+            .AddDefaultTokenProviders()
                .AddEntityFrameworkStores<AppDbContext>();
 
             services.AddScoped<IPieRepository, PieRepository>();
@@ -64,11 +65,11 @@ namespace OnlineCakeShop
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
 
-            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+         /*   using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
             {
                 var context = serviceScope.ServiceProvider.GetRequiredService<AppDbContext>();
                 context.Database.Migrate();
-            }
+            }*/
 
             if (env.IsDevelopment())
             {
