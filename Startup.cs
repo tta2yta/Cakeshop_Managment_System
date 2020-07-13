34725cfcdf5a7169d62262569ef4d491c6e36110
tta2yta@gmail.com <tta2yta@gmail.com>
2020-07-13 12:19:38 +0300

    modify logout and shoppingCartSummary viewcomponent

--- Startup.cs
+++ Startup.cs
@@ -27,15 +27,15 @@ namespace OnlineCakeShop
         }
         public void ConfigureServices(IServiceCollection services)
         {
-            /*services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));*/
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
 
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
-            {
-                services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
-            }
+            /*   if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+               {
+                   services.AddDbContext<AppDbContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
+               }*/
 
 
             services.AddIdentity<IdentityUser, IdentityRole>(options =>
