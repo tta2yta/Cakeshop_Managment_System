5598bfb56739db275254d29c9dd08583293a181b
tta2yta@gmail.com <tta2yta@gmail.com>
2020-07-13 18:39:58 +0300

    modify viewmodel folder 7/13/2020

--- Model/ViewModels/UserRoleViewModel.cs
+++ Model/ViewModels/UserRoleViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,13 @@ namespace OnlineCakeShop.Model.ViewModels
 {
     public class UserRoleViewModel
     {
+        public UserRoleViewModel()
+        {
+            Users = new List<IdentityUser>();
+        }
+        public string UserId { get; set; }
+        public string RoleId { get; set; }
+        public List<IdentityUser> Users { get; set; }
+
     }
 }
